e7a1df660c4918ed9d5a2543745b546936b5e6f0
Nirmalkumar9182 <thanign@sph.com.sg>
2023-12-11 23:27:43 +0800

    adding StyleX to about and contact page

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -1,9 +1,22 @@
 import { ReactElement } from "react"
+import * as stylex from "@stylexjs/stylex"
 
 export function About(): ReactElement {
   return (
     <>
-      <h1>About Page</h1>
+      <h1>
+        <div {...stylex.props(styles.violet)}>About Page</div>
+      </h1>
     </>
   )
 }
+
+const styles = stylex.create({
+  violet: {
+    backgroundColor: {
+      default: "blueviolet",
+      ":hover": "darkviolet",
+    },
+    color: "white",
+  },
+})
--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -1,9 +1,22 @@
+import stylex from "@stylexjs/stylex"
 import { ReactElement } from "react"
 
 export function Contact(): ReactElement {
   return (
     <>
-      <h1>Contact Page</h1>
+      <h1>
+        <div {...stylex.props(styles.green)}>Contact Page</div>
+      </h1>
     </>
   )
 }
+
+const styles = stylex.create({
+  green: {
+    backgroundColor: {
+      default: "lightgreen",
+      ":hover": "darkgreen",
+    },
+    color: "black",
+  },
+})
