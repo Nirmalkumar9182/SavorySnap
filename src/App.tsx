93357354bd92a47a4f0e2ca2915666b73c301331
Nirmalkumar9182 <thanign@sph.com.sg>
2023-12-11 23:32:27 +0800

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,14 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import { Home } from "./app/pages/Home"
 import { About } from "./app/pages/About"
 import { Contact } from "./app/pages/Contact"
+import { NavBar } from "./app/componenets/NavBar"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
 
           <Routes>
             <Route path="/" element={<Home />} />
---
+++ src/app/componenets/NavBar.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react"
+
+export function NavBar(): ReactElement {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
